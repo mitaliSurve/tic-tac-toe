d7736499bbbf9b92484a74ab1a9474b756ee7b6e
mitali28 <mitalisurve2@gmail.com>
2021-05-21 18:14:38 +0530

    add gaming functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <lable>Tic-Tac-Toe</lable>
+        <Game />
       </header>
     </div>
   );
